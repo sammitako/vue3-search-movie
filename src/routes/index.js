38a03d66f87cda8f449d0407856c9cc15117e5aa
sammitako <p.hansaem@hotmail.com>
2022-06-28 15:24:25 +0900

    Setup: Vue-router

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./Home";
+import About from "./About";
+
+export default createRouter({
+  // Hash, History 모드
+  // Hash 모드 -> https://google.com/#/search
+  history: createWebHashHistory(),
+  // pages
+  // path: https://google.com/ -> 메인 페이지
+  // component: '/' 뒤에 연결할 컴포넌트
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/about",
+      component: About,
+    },
+  ],
+});
