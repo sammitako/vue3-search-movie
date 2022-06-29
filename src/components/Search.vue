188d1dd5a337861b65007223edd6c95d3c1c8391
sammitako <p.hansaem@hotmail.com>
2022-06-29 11:37:56 +0900

    Feat: select and option tag

---
+++ src/components/Search.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <input
+      v-model="title"
+      class="form-control"
+      type="text"
+      placeholder="Search for Movies, Series & more"
+    />
+    <div class="selects">
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]"
+        :key="filter.name"
+        class="form-select"
+      >
+        <option value="" v-if="filter.name === 'year'">All Years</option>
+        <option v-for="item in filter.items" :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      type: "movie",
+      number: 10,
+      year: "",
+      filters: [
+        {
+          name: "type",
+          items: ["movie", "series", "episode"],
+        },
+        {
+          name: "number",
+          items: [10, 20, 30],
+        },
+        {
+          name: "year",
+          // 즉시실행 함수
+          items: (() => {
+            const years = [];
+            const thisYear = new Date().getFullYear();
+            for (let i = thisYear; i >= 1985; i -= 1) {
+              years.push(i);
+            }
+            return years;
+          })(),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  > * {
+    margin-right: 10px;
+    font-size: 15px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  .selects {
+    display: flex;
+    select {
+      width: 120px;
+      margin-right: 10px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
