5f01c2f522a74cffbf51ea9df1f48af0e1de4cc6
sammitako <p.hansaem@hotmail.com>
2022-07-01 10:54:06 +0900

    Style: animated posters

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -35,6 +35,15 @@ export default {
   background-size: cover;
   overflow: hidden;
   position: relative;
+  &:hover::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border: 6px solid $primary;
+  }
   .info {
     background-color: rgba($black, 0.3);
     width: 100%;
@@ -44,6 +53,16 @@ export default {
     position: absolute;
     left: 0;
     bottom: 0;
+    backdrop-filter: blur(10px);
+    .year {
+      color: $primary;
+    }
+    .title {
+      color: $white;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
   }
 }
 </style>
