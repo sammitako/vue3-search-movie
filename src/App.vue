8cdc73a5edfaecd1e06dbc8de02db660d8ae3222
sammitako <p.hansaem@hotmail.com>
2022-07-01 14:04:46 +0900

    Feat: added footer component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 <script>
 import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
