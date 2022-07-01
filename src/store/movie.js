d8397feb7a2645d169ada9eda400df25df031461
sammitako <p.hansaem@hotmail.com>
2022-07-01 09:40:31 +0900

    Refactor: used asynchronous pattern -> promise

--- src/store/movie.js
+++ src/store/movie.js
@@ -24,11 +24,10 @@ export default {
   },
   actions: {
     async searchMovies(context, payload) {
-      const { title, type, number, year } = payload;
-      const OMDB_API_KEY = "1763cb00";
-      const res = await axios.get(
-        `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-      );
+      const res = await _fetchMovie({
+        ...payload,
+        page: 1,
+      });
       const { Search, totalResults } = res.data;
       context.commit("updateState", {
         movies: _uniqBy(Search, "imdbID"), // 첫 10개 영화 목록
@@ -39,10 +38,11 @@ export default {
       // 데이터 추가 요청
       if (pageLength > 1) {
         for (let page = 2; page <= pageLength; page += 1) {
-          if (page > number / 10) break;
-          const res = await axios.get(
-            `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-          );
+          if (page > payload.number / 10) break;
+          const res = await _fetchMovie({
+            ...payload,
+            page,
+          });
           const { Search } = res.data;
           context.commit("updateState", {
             movies: [...context.state.movies, ..._uniqBy(Search, "imdbID")], // 첫 10개 영화 목록 + 나중에 추가된 영화 목록
@@ -52,3 +52,20 @@ export default {
     },
   },
 };
+
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload;
+  const OMDB_API_KEY = "1763cb00";
+  const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+}
