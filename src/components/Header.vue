84454b37352bb86346aea1c408ba0167225f9178
sammitako <p.hansaem@hotmail.com>
2022-07-05 11:54:59 +0900

    Style: logo on the right

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,9 @@
         </RouterLink>
       </div>
     </div>
+    <div class="user">
+      <img :src="image" :alt="name" />
+    </div>
   </header>
 </template>
 
@@ -38,6 +41,14 @@ export default {
       ],
     };
   },
+  computed: {
+    image() {
+      return this.$store.state.about.image;
+    },
+    name() {
+      return this.$store.state.about.name;
+    },
+  },
   methods: {
     isMatch(path) {
       if (!path) return false; // Search, About
@@ -49,14 +60,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
 header {
   height: 70px;
   padding: 0 40px;
   display: flex;
   align-items: center;
+  position: relative;
   /* Header 안에 있는 로고 */
   .logo {
     margin-right: 40px;
+  }
+  .user {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    cursor: pointer;
+    overflow: hidden;
+    border: 3px solid $gray-200;
+    /* 수직 가운데 */
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    right: 40px;
+    &:hover {
+      border: 3px solid darken($gray-200, 10%);
+    }
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
   }
 }
 </style>
